955d22a92bd64b7c6fd08c1f302f28191921a77d
Anand Pandey <anand.pandey@us.imshealth.com>
2020-09-02 07:43:40 -0400

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,455 @@
-import React from "react";
+import React, { useState, MouseEvent } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Board } from "./Board";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Grid,
+  IconButton,
+  Tooltip,
+  Popper,
+  Fade,
+  Paper,
+  ClickAwayListener,
+  Slider,
+  Divider,
+  Button,
+} from "@material-ui/core";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import {
+  Crop169 as Crop169Icon,
+  RadioButtonUnchecked as RadioButtonUncheckedIcon,
+  BorderColor as BorderColorIcon,
+  OpenWith as OpenWithIcon,
+  CropSquare as CropSquareIcon,
+  GroupAdd as GroupAddIcon,
+  Palette as PaletteIcon,
+  Undo as UndoIcon,
+  Redo as RedoIcon,
+  TextFields as TextFieldsIcon,
+  TextFormat as TextFormatIcon,
+} from "@material-ui/icons";
+import { ReactComponent as EraserIcon } from "assets/images/eraser.svg";
+import { ReactComponent as EllipseIcon } from "assets/images/ellipse-outline.svg";
+import { CirclePicker, ColorResult } from "react-color";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    pageContainer: {
+      // minHeight: "100vh",
+    },
+    title: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    appBar: {
+      background: `linear-gradient(90deg,${"#222"} 0%,${"#333"} 40%,${"#555"} 100%)`,
+    },
+    customizeToolbar: {
+      // minHeight: 10,
+      // paddingTop: 10,
+    },
+    leftMenu: {
+      width: 50,
+      height: "100vh",
+      background: "#222",
+      // border: "1px solid red",
+      zIndex: 1,
+    },
+    board: {
+      flexGrow: 1,
+      // border: "1px solid blue",
+    },
+    iconButton: {
+      fill: "#fff",
+    },
+    swatchContainer: {
+      padding: theme.spacing(2),
+      backgroundColor: "#484848",
+    },
+    toggleRoot: {
+      color: "#ff0000",
+
+      // "&:hover": {
+      //   backgroundColor: "#ff0000",
+      // },
+      // backgroundColor: "#ff0000",
+    },
+    markLabel: {
+      color: "#fff",
+      fontSize: 10,
+    },
+    valueLabel: {
+      // color: "#222",
+      "& *": {
+        background: "#fff",
+        color: "#222",
+      },
+    },
+  })
+);
+
+const toggleStyles = makeStyles(
+  {
+    root: {
+      backgroundColor: "#222",
+      "&:hover": {
+        backgroundColor: "#ff0000",
+      },
+      // "&:focus": {
+      //   backgroundColor: "#c4c4c4",
+      // },
+      "&:active": {
+        backgroundColor: "blue",
+      },
+      "&:visited": {
+        backgroundColor: "green",
+      },
+    },
+    label: {
+      /* … */
+    },
+    outlined: {
+      /* … */
+      "&$disabled": {
+        /* … */
+      },
+    },
+    outlinedPrimary: {
+      /* … */
+      "&:hover": {
+        color: "#ff0000",
+      },
+    },
+    disabled: {},
+  },
+  { name: "MuiButton" }
+);
 
 export const App = () => {
-  return <Board />;
+  const classes = useStyles();
+  // const toggleClass = toggleStyles();
+
+  const [mode, setMode] = useState("brush");
+  const [color, setColor] = useState<string>("#f44336");
+
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [open, setOpen] = useState(false);
+
+  const [sliderValue, setSliderValue] = useState<number>(2);
+
+  // console.log("mode--->", mode, color);
+  const handleToggleChange = (
+    event: MouseEvent<HTMLElement>,
+    nextView: string
+  ) => {
+    setMode(nextView);
+    if (nextView === "brush") {
+      setSliderValue(2);
+    } else if (nextView === "erase") {
+      setSliderValue(10);
+    }
+  };
+
+  const handleColorChange = (color: ColorResult) => {
+    setColor(color.hex);
+  };
+
+  const handleOpenPopper = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(!open);
+  };
+
+  const handleSliderChange = (event: any, newValue: number | number[]) => {
+    setSliderValue(newValue as number);
+  };
+
+  return (
+    <div className={classes.pageContainer}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar variant="dense" className={classes.customizeToolbar}>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton> */}
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            Scrib.ink
+          </Typography>
+          {/* <Tooltip title="Invite Others" aria-label="Invite Others">
+            <IconButton>
+              
+            </IconButton>
+          </Tooltip> */}
+          <Button
+            startIcon={<GroupAddIcon className={classes.iconButton} />}
+            style={{ color: "#fff" }}
+          >
+            Invite Others
+          </Button>
+          <Button style={{ color: "#fff" }}>Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Grid container direction="row">
+        <Grid item className={classes.leftMenu}>
+          <ToggleButtonGroup
+            orientation="vertical"
+            value={mode}
+            exclusive
+            onChange={handleToggleChange}
+          >
+            <Tooltip title="Move" aria-label="Move" placement="right">
+              <ToggleButton
+                value="move"
+                aria-label="move"
+                style={{
+                  backgroundColor: mode === "move" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <OpenWithIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip title="Pen" aria-label="Pen" placement="right">
+              <ToggleButton
+                value="brush"
+                aria-label="brush"
+                style={{
+                  backgroundColor: mode === "brush" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <BorderColorIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip
+              title="Drop TextField"
+              aria-label="Drop TextField"
+              placement="right"
+            >
+              <ToggleButton
+                value="textField"
+                aria-label="textField"
+                style={{
+                  backgroundColor: mode === "textField" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <TextFieldsIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip title="Text Tool" aria-label="Text Tool" placement="right">
+              <ToggleButton
+                value="textTool"
+                aria-label="textTool"
+                style={{
+                  backgroundColor: mode === "textTool" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <TextFormatIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip title="Erase" aria-label="Erase" placement="right">
+              <ToggleButton
+                value="erase"
+                aria-label="erase"
+                style={{
+                  backgroundColor: mode === "erase" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <EraserIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip
+              title="Draw Square"
+              aria-label="Draw Square"
+              placement="right"
+            >
+              <ToggleButton
+                value="square"
+                aria-label="square"
+                style={{
+                  backgroundColor: mode === "square" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <CropSquareIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip
+              title="Draw Rectangle"
+              aria-label="Draw Rectangle"
+              placement="right"
+            >
+              <ToggleButton
+                value="rect"
+                aria-label="rect"
+                style={{
+                  backgroundColor: mode === "rect" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <Crop169Icon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip
+              title="Draw Circle"
+              aria-label="Draw Circle"
+              placement="right"
+            >
+              <ToggleButton
+                value="circle"
+                aria-label="circle"
+                style={{
+                  backgroundColor: mode === "circle" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <RadioButtonUncheckedIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+            <Tooltip
+              title="Draw Ellipse"
+              aria-label="Draw Ellipse"
+              placement="right"
+            >
+              <ToggleButton
+                value="ellipse"
+                aria-label="ellipse"
+                style={{
+                  backgroundColor: mode === "ellipse" ? "#7a7a7a" : "inherit",
+                }}
+              >
+                <EllipseIcon className={classes.iconButton} />
+              </ToggleButton>
+            </Tooltip>
+          </ToggleButtonGroup>
+          <Tooltip title="Undo" aria-label="Undo" placement="right">
+            <IconButton onClick={() => {}}>
+              <UndoIcon className={classes.iconButton} />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Redo" aria-label="Redo" placement="right">
+            <IconButton onClick={() => {}}>
+              <RedoIcon className={classes.iconButton} />
+            </IconButton>
+          </Tooltip>
+          {mode !== "move" && (
+            <Tooltip
+              title="Choose Color"
+              aria-label="Choose Color"
+              placement="right"
+            >
+              <IconButton onClick={handleOpenPopper}>
+                <PaletteIcon
+                  className={classes.iconButton}
+                  style={{
+                    fill: color,
+                    backgroundColor:
+                      color === "#000000" ? "#7a7a7a" : "inherit",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+          )}
+          <Divider style={{ backgroundColor: "#535353", marginTop: 5 }} />
+          {["brush", "erase", "textTool"].includes(mode) && (
+            <div
+              style={{
+                width: 50,
+                // border: "1px solid red",
+                height: 100,
+                marginTop: 20,
+              }}
+            >
+              <Slider
+                value={sliderValue}
+                valueLabelDisplay="auto"
+                onChange={handleSliderChange}
+                orientation="vertical"
+                defaultValue={sliderValue}
+                aria-labelledby="vertical-slider"
+                min={1}
+                max={mode === "erase" ? 100 : 10}
+                style={{ marginLeft: 10, color: "#fff" }}
+                classes={{
+                  // markLabel: classes.markLabel,
+                  valueLabel: classes.valueLabel,
+                }}
+                // marks={[
+                //   {
+                //     value: 1,
+                //     label: 1,
+                //   },
+                //   {
+                //     value: 30,
+                //     label: 30,
+                //   },
+                // ]}
+              />
+            </div>
+          )}
+        </Grid>
+        <Grid item className={classes.board} xs={11}>
+          <Board mode={mode} color={color} sliderValue={sliderValue} />
+        </Grid>
+      </Grid>
+
+      <Popper
+        open={open}
+        anchorEl={anchorEl}
+        placement={"left"}
+        transition
+        disablePortal={false}
+        modifiers={{
+          flip: {
+            enabled: true,
+          },
+          preventOverflow: {
+            enabled: true,
+            boundariesElement: "scrollParent",
+          },
+          // arrow: {
+          //   enabled: true,
+          //   element: arrowRef,
+          // },
+        }}
+      >
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper
+              className={classes.swatchContainer}
+              onMouseLeave={() => setOpen(false)}
+            >
+              <ClickAwayListener onClickAway={() => setOpen(false)}>
+                <CirclePicker
+                  color={color}
+                  onChangeComplete={handleColorChange}
+                  colors={[
+                    "#f44336",
+                    "#e91e63",
+                    "#9c27b0",
+                    "#673ab7",
+                    "#3f51b5",
+                    "#2196f3",
+                    "#03a9f4",
+                    "#00bcd4",
+                    "#009688",
+                    "#4caf50",
+                    "#8bc34a",
+                    "#cddc39",
+                    "#ffeb3b",
+                    "#ffc107",
+                    "#ff9800",
+                    "#ff5722",
+                    "#795548",
+                    "#000000",
+                  ]}
+                />
+              </ClickAwayListener>
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
+    </div>
+  );
 };
--- src/Board.tsx
+++ src/Board.tsx
@@ -7,15 +7,37 @@ import React, {
   useReducer,
   FC,
 } from "react";
+import "./Board.css";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Stage, Layer, Line, Text, Image } from "react-konva";
 import Konva from "konva";
 import { Grid, IconButton } from "@material-ui/core";
 import { ReactComponent as EraserIcon } from "assets/images/eraser.svg";
 import { ReactComponent as AddTextIcon } from "assets/images/format-text.svg";
-import { Create as CreateIcon } from "@material-ui/icons";
+import {
+  Create as CreateIcon,
+  ViewList as ViewListIcon,
+  ViewModule as ViewModuleIcon,
+  ViewQuilt as ViewQuiltIcon,
+} from "@material-ui/icons";
 import { findLastIndex, cloneDeep } from "lodash";
 import useImage from "use-image";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import classes from "*.module.css";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    // bckGrid: {
+    //   backgroundImage:
+    //     "radial-gradient(circle at 1px 1px,#dfe2e6 1px,transparent 0)",
+    //   backgroundSize: "12px 12px",
+    //   backgroundAttachment: "fixed",
+    //   height: "100%",
+    //   width: "96vw",
+    // },
+  })
+);
 const CustomImage: FC<any> = memo((props) => {
   const { id, src, handleDragStart, handleDragEnd } = props;
   const [image] = useImage(src);
@@ -40,24 +62,25 @@ interface IObject {
 interface IBoardState {
   objects: IObject[];
   isPaint: boolean;
-  mode: string;
+  //   mode: string;
+  selectedObjId?: string;
 }
 const initialState: IBoardState = {
   isPaint: false,
-  mode: "brush",
+  //   mode: "brush",
   objects: [
-    {
-      type: "text",
-      property: {
-        id: Date.now().toString(),
-        text: "Scrib.ink",
-        x: 10,
-        y: 10,
-        fontSize: 20,
-        draggable: true,
-        width: 200,
-      },
-    },
+    // {
+    //   type: "text",
+    //   property: {
+    //     id: Date.now().toString(),
+    //     text: "Scrib.ink",
+    //     x: 10,
+    //     y: 10,
+    //     fontSize: 20,
+    //     draggable: true,
+    //     width: 200,
+    //   },
+    // },
   ],
 };
 const reducer = (state: IBoardState, action: any): IBoardState => {
@@ -80,10 +103,16 @@ const reducer = (state: IBoardState, action: any): IBoardState => {
         isPaint: action.payload,
       };
     }
-    case "setMode": {
+    // case "setMode": {
+    //   return {
+    //     ...state,
+    //     mode: action.payload,
+    //   };
+    // }
+    case "sselectObject": {
       return {
         ...state,
-        mode: action.payload,
+        selectedObjId: action.payload,
       };
     }
     default: {
@@ -92,7 +121,14 @@ const reducer = (state: IBoardState, action: any): IBoardState => {
   }
 };
 
-export const Board: FC = () => {
+interface IBoardProps {
+  mode: string;
+  color: string;
+  sliderValue: number;
+}
+export const Board: FC<IBoardProps> = (props) => {
+  const classes = useStyles();
+
   const [state, dispatch] = useReducer(reducer, cloneDeep(initialState));
 
   const setState = (type: string, payload?: any) => {
@@ -106,10 +142,10 @@ export const Board: FC = () => {
 
   const [cursor, setCursor] = useState<string>("default");
 
-  const cursors = {
-    text: "text",
-    line: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAABh0lEQVRIS7XVv2vCQBQH8PdwEYdO2SJFcAtSkiFubY0UUvIfhOLi0CVzUQhOilrsf9Ct2NKlqzSkkO5CoZZkECQW7OrQwUFor1whJfFXjdGb7z7v3YP7HsKOFnouIYRFxI9t1fHDtcFgoLIsex+Px28R0YlSxA9z1WrVbrVaUCgUQFGUkSRJN4lE4hIRP8MW+YPpwW63+y6K4n65XIZ2uw3T6RQMw3jlef4oLB6ACSG1dDqt064rlQo4jgMMw3ybpvkWFp+FZV3XH+v1euDmm+ABmGqu675IknQwHA5jfj0sPgcTQvZc133O5/OR8DmYdrkNfCG8DXwpHBVfCUfB/4XXwS3L6mUymWP/I1oLXoWnUqmvYrEYk2W5l81mDz18bXgRTlFVVWONRgM4jgPbts8R8ZruDQX7cU3TBJ7ngaK+6L1DxLONYA+fTCZPgiCI/X7/1202m1AqlU4R0dgY9vDxePzQ6XROkskk5HK5C0S88roPPYrZXCaEcAAwmo3VyPCyD2Bn8A8S2gEmQxchowAAAABJRU5ErkJggg==") 4 4, auto`,
-  };
+  //   const cursors = {
+  //     text: "text",
+  //     line: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAABh0lEQVRIS7XVv2vCQBQH8PdwEYdO2SJFcAtSkiFubY0UUvIfhOLi0CVzUQhOilrsf9Ct2NKlqzSkkO5CoZZkECQW7OrQwUFor1whJfFXjdGb7z7v3YP7HsKOFnouIYRFxI9t1fHDtcFgoLIsex+Px28R0YlSxA9z1WrVbrVaUCgUQFGUkSRJN4lE4hIRP8MW+YPpwW63+y6K4n65XIZ2uw3T6RQMw3jlef4oLB6ACSG1dDqt064rlQo4jgMMw3ybpvkWFp+FZV3XH+v1euDmm+ABmGqu675IknQwHA5jfj0sPgcTQvZc133O5/OR8DmYdrkNfCG8DXwpHBVfCUfB/4XXwS3L6mUymWP/I1oLXoWnUqmvYrEYk2W5l81mDz18bXgRTlFVVWONRgM4jgPbts8R8ZruDQX7cU3TBJ7ngaK+6L1DxLONYA+fTCZPgiCI/X7/1202m1AqlU4R0dgY9vDxePzQ6XROkskk5HK5C0S88roPPYrZXCaEcAAwmo3VyPCyD2Bn8A8S2gEmQxchowAAAABJRU5ErkJggg==") 4 4, auto`,
+  //   };
 
   const pasteImage = (e: Event) => {
     const { clipboardData } = e as ClipboardEvent;
@@ -135,6 +171,30 @@ export const Board: FC = () => {
   };
 
   useEffect(() => {
+    switch (props.mode) {
+      case "move": {
+        setCursor("move");
+        break;
+      }
+      case "brush": {
+        setCursor(
+          `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAABh0lEQVRIS7XVv2vCQBQH8PdwEYdO2SJFcAtSkiFubY0UUvIfhOLi0CVzUQhOilrsf9Ct2NKlqzSkkO5CoZZkECQW7OrQwUFor1whJfFXjdGb7z7v3YP7HsKOFnouIYRFxI9t1fHDtcFgoLIsex+Px28R0YlSxA9z1WrVbrVaUCgUQFGUkSRJN4lE4hIRP8MW+YPpwW63+y6K4n65XIZ2uw3T6RQMw3jlef4oLB6ACSG1dDqt064rlQo4jgMMw3ybpvkWFp+FZV3XH+v1euDmm+ABmGqu675IknQwHA5jfj0sPgcTQvZc133O5/OR8DmYdrkNfCG8DXwpHBVfCUfB/4XXwS3L6mUymWP/I1oLXoWnUqmvYrEYk2W5l81mDz18bXgRTlFVVWONRgM4jgPbts8R8ZruDQX7cU3TBJ7ngaK+6L1DxLONYA+fTCZPgiCI/X7/1202m1AqlU4R0dgY9vDxePzQ6XROkskk5HK5C0S88roPPYrZXCaEcAAwmo3VyPCyD2Bn8A8S2gEmQxchowAAAABJRU5ErkJggg==") 4 4, auto`
+        );
+        break;
+      }
+      case "erase": {
+        setCursor(
+          `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAACDElEQVRIS7XUsYvaUBwH8N+vdbLU5g+oCE5SqVIo5TqVUHC4JZKgRkGQgqDFQeo5ODgJgXqIB1JqTa2LgikqOEnBrqVjI203jUudvR6nOEjKkwuIjRfPu8v28h6f3zcvv/cQbunBTVdVVYuiKJ+Gw+EzmqbTJpNJ2qe2HnzkdruPB4MB+P1+qFQqHyiKil0V14UtFssxy7LQ6XTA5XJBo9Go2my2N4j4d9cCW2FRFMHhcJx6PJ4HFEWRIj+cTueLXfFL4UAg4ByPx3WGYZ4oigLdbvcnTdMBRPxtlPxSmOd5JD9TkqRTnudXVrvdPmNZ9sAIN4QJ1mw2VQ0m42KxeJ5MJl8h4udtyfeCCZZKpSCXywXNZnNTD98KFwoFiEajTvLJm4k1yOfzgSiKuu2oB0fj8XilXC5Dq9U64zjuQJKkX+tbsZ5QEATIZDKrAOvv/4PJ5Hw+f59IJGLVahXy+fy51Wq9FwwGdbczEolArVZ7jojfDWGyYLlcvhME4XU2mwW73Q6j0ehmYKKoquqv1+tSOBze2rZXTqxJBO/3+x8ZhjHPZrO7mxX2hi+SP5Jl+dvh4eH9yWRyZx2/Fqzh5Hh7vd7HsiybNPza8AVumU6nX0Oh0NNer7eybwTW8MVi8TadTsdKpRLpdeA47iEi/tmp3YxuL1VVjwDgJQCcIOKXzfW6B8QI3WX+1uB/2SsKJtnXa2AAAAAASUVORK5CYII=") 4 4, auto`
+        );
+        break;
+      }
+      default: {
+        setCursor("default");
+        break;
+      }
+    }
+  }, [props.mode]);
+  useEffect(() => {
     window.addEventListener("paste", pasteImage);
 
     return () => {
@@ -143,44 +203,51 @@ export const Board: FC = () => {
   }, []);
 
   const handleDown = () => {
-    setState("setIsPaint", true);
-    var pos = ((stageRef.current as unknown) as Konva.Stage).getPointerPosition();
-    if (pos !== null) {
-      const line = {
-        type: "line",
-        property: {
-          id: Date.now().toString(),
-          stroke: "#ff0000",
-          strokeWidth: 2,
-          globalCompositeOperation:
-            state.mode === "brush" ? "source-over" : "destination-out",
-          points: [pos.x, pos.y],
-        },
-      };
-      setCursor(cursors.line);
+    if (["brush", "erase"].includes(props.mode)) {
+      setState("setIsPaint", true);
+      var pos = ((stageRef.current as unknown) as Konva.Stage).getPointerPosition();
+      if (pos !== null) {
+        const line = {
+          type: "line",
+          property: {
+            id: Date.now().toString(),
+            stroke: props.color,
+            strokeWidth: props.sliderValue,
+            globalCompositeOperation:
+              props.mode === "brush" ? "source-over" : "destination-out",
+            points: [pos.x, pos.y],
+          },
+        };
 
-      setState("addObjects", line);
+        setState("addObjects", line);
+      }
     }
   };
 
   const handleUp = () => {
-    setState("setIsPaint", false);
-    setCursor("default");
+    if (["brush", "erase"].includes(props.mode)) {
+      setState("setIsPaint", false);
+    }
   };
 
   const handleMove = () => {
-    if (!state.isPaint) {
-      return;
-    }
+    if (["brush", "erase"].includes(props.mode)) {
+      if (!state.isPaint) {
+        return;
+      }
 
-    const pos = ((stageRef.current as unknown) as Konva.Stage).getPointerPosition();
-    if (pos !== null) {
-      const newObjects = state.objects.slice();
-      const lineIndex = findLastIndex(state.objects, (o) => o.type === "line");
-      let line = state.objects[lineIndex];
-      line.property.points = line.property.points.concat([pos.x, pos.y]);
-      newObjects.splice(lineIndex, 1, line);
-      setState("setObjects", newObjects);
+      const pos = ((stageRef.current as unknown) as Konva.Stage).getPointerPosition();
+      if (pos !== null) {
+        const newObjects = state.objects.slice();
+        const lineIndex = findLastIndex(
+          state.objects,
+          (o) => o.type === "line"
+        );
+        let line = state.objects[lineIndex];
+        line.property.points = line.property.points.concat([pos.x, pos.y]);
+        newObjects.splice(lineIndex, 1, line);
+        setState("setObjects", newObjects);
+      }
     }
   };
 
@@ -199,11 +266,11 @@ export const Board: FC = () => {
         draggable: true,
         width: 200,
         onDragStart: () => {
-          setCursor("move");
+          //   setCursor("move");
           // setIsDragging(true);
         },
         onDragEnd: (e: any) => {
-          setCursor("default");
+          //   setCursor("default");
           // setIsDragging(false);
         },
       },
@@ -233,71 +300,46 @@ export const Board: FC = () => {
     // Stage - is a div wrapper
     // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
     // Rect and Circle are not DOM elements. They are 2d shapes on canvas
-    <Grid container direction="row">
-      <Grid
-        item
-        style={{
-          border: "0px solid red",
-          width: "100px",
-          backgroundColor: "#222", // "#4c4c4c",
-        }}
-      >
-        <EraserIcon />
-        <CreateIcon />
-        <IconButton onClick={handleAddText}>
-          <AddTextIcon />
-        </IconButton>
 
-        <select onChange={handleChange}>
-          <option value="brush">Brush</option>
-          <option value="eraser">Eraser</option>
-        </select>
-      </Grid>
-      <Grid item xs={11}>
-        <Stage
-          style={{
-            backgroundImage:
-              "radial-gradient(circle at 1px 1px,#dfe2e6 1px,transparent 0)",
-            backgroundSize: "12px 12px",
-            backgroundAttachment: "fixed",
-            height: "100%",
-            cursor: cursor,
-          }}
-          ref={stageRef}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          onMouseDown={handleDown}
-          onTouchStart={handleDown}
-          onMouseUp={handleUp}
-          onTouchEnd={handleUp}
-          onMouseMove={handleMove}
-          onTouchMove={handleMove}
-        >
-          <Layer>
-            {state.objects
-              .filter((e) => e.type === "line")
-              ?.map((line: IObject, index: number) => (
-                <Line key={index} {...line.property} />
-              ))}
-            {state.objects
-              .filter((e) => e.type === "text")
-              ?.map((text: IObject, index: number) => (
-                <Text key={index} {...text.property} />
-              ))}
-            {state.objects
-              .filter((e) => e.type === "image")
-              ?.map((image: IObject, index: number) => (
-                <CustomImage
-                  key={index}
-                  id={image.property.id}
-                  src={image.property.src}
-                  handleDragStart={handleDragStart(image.property.id)}
-                  handleDragEnd={handleDragEnd(image.property.id)}
-                />
-              ))}
-          </Layer>
-        </Stage>
-      </Grid>
-    </Grid>
+    <Stage
+      className={"board"}
+      style={{
+        // marginLeft: -50,
+        cursor: cursor,
+      }}
+      ref={stageRef}
+      width={window.innerWidth - 100}
+      height={window.innerHeight}
+      onMouseDown={handleDown}
+      onTouchStart={handleDown}
+      onMouseUp={handleUp}
+      onTouchEnd={handleUp}
+      onMouseMove={handleMove}
+      onTouchMove={handleMove}
+    >
+      <Layer>
+        {state.objects
+          .filter((e) => e.type === "line")
+          ?.map((line: IObject, index: number) => (
+            <Line key={index} {...line.property} />
+          ))}
+        {state.objects
+          .filter((e) => e.type === "text")
+          ?.map((text: IObject, index: number) => (
+            <Text key={index} {...text.property} />
+          ))}
+        {state.objects
+          .filter((e) => e.type === "image")
+          ?.map((image: IObject, index: number) => (
+            <CustomImage
+              key={index}
+              id={image.property.id}
+              src={image.property.src}
+              handleDragStart={handleDragStart(image.property.id)}
+              handleDragEnd={handleDragEnd(image.property.id)}
+            />
+          ))}
+      </Layer>
+    </Stage>
   );
 };
